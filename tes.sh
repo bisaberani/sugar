f3cde6d7e36c0bcce2c6a36a73318afc4285a7ec
bisaberani <83889430+bisaberani@users.noreply.github.com>
2021-05-09 06:45:14 +0700

    Create tes.sh

---
+++ tes.sh
@@ -0,0 +1,10 @@
+wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-linux64.zip
+ 
+
+    unzip sugarmaker-v2.5.0-sugar4-linux64.zip
+
+
+    cd sugarmaker-v2.5.0-sugar4-linux64
+
+
+   ./sugarmaker -a YespowerSugar -o stratum+tcp://stratum-eu.rplant.xyz:7042 -u sugar1qvgr0ww6fl5dyxkl30tr0y4g0x0vgm3wcmaw73y.vps1 -t1
